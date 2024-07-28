627db3215de6ce99869bd963d3b4b29a24fcef94
TheDiamondOG <xanderfpowell@gmail.com>
2024-07-28 19:08:43 -0400

    Testing automation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pastebinlib',
-    version='1.1.2',
+    version='1.1.3',
     packages=find_packages(),
     install_requires=['requests'],
     description='This is a library for accessing pastebin easily. Also pastelib was already taken',
