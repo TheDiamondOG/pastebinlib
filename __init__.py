49d225161a4c18d846c4fc9dfcdc277ece5a83ae
TheDiamondOG <xanderfpowell@gmail.com>
2024-07-28 18:56:38 -0400

    Fixed this to be on pypi

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 # pastelib/__init__.py
 
-from .pastelib import set_api_key, create_paste, get_paste, get_paste_id, delete_paste
+from .pastebinlib import set_api_key, create_paste, get_paste, get_paste_id, delete_paste
--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pastelib',
+    name='pastebinlib',
     version='1.0',
     packages=find_packages(),
     install_requires=['requests'],
-    description='This is a library for accessing pastebin easly.',
+    description='This is a library for accessing pastebin easly. Also pastelib was token',
+long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='TheDiamondOG',
     author_email='thediamondogness@gmail.com',
     url='https://github.com/thediamondog/pastelib',
